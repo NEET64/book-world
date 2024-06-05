f156ac4e94849668820f808dec87853be19b9597
Neet <neetdhameliya321@gmail.com>
2024-06-06 04:45:45 +0530

    fix: minor

--- frontend/src/components/NotFound.jsx
+++ frontend/src/components/NotFound.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 const NotFound = () => {
   return (
-    <section className="bg-white min-h-screen dark:bg-gray-900 dark:text-zinc-50 w-full">
+    <section className="bg-white min-h-screen dark:bg-zinc-900 dark:text-zinc-50 w-full">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
         <div className="mx-auto max-w-screen-sm text-center">
           <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600">
@@ -11,13 +11,13 @@ const NotFound = () => {
           <p className="mb-4 text-3xl tracking-tight font-bold ">
             Page Not Found.
           </p>
-          <p className="mb-4 text-lg font-light text-gray-500">
+          <p className="mb-4 text-lg font-light text-zind-500">
             Sorry, we can't find that page. You'll find lots to explore on the
             home page.
           </p>
           <Link
             to="/books"
-            className="inline-flex text-white bg-gray-800 hover:bg-gray-800/90 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
+            className="inline-flex text-white bg-zinc-700 hover:bg-zind-800/90 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900 my-4">
             Back to Homepage
           </Link>
         </div>
